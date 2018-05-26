07dd3f1bdfc4bfe451a2cdf4db83d8aeb3c90f81
ZalkinV <Zalkin.viktor@mail.ru>
2018-05-26 13:40:27 +0300

    Additions connected with writing data in file and logging in the system
    1. Создал класс для команд от пользователя, чтобы с ними было проще работать
    2. Переписал метод для создания курсов
    3. Запись и считывание данных о курсах и пользователях в файл работает правильно (надеюсь)
    4. Пользователи теперь должны входить в свои аккаунты, чтобы взаимодействовать с системой
    5. Добавил воможность входить в систему в методе Login

--- PCMConsoleApp/PCMConsoleApp/CourseClass.py
+++ PCMConsoleApp/PCMConsoleApp/CourseClass.py
@@ -1,14 +1,14 @@
 from enum import Enum
 
 class Course:
-	def __init__(self, name, startDate, endDate):
+	def __init__(self, name, startDate, endDate, creator):
 		self.__name = name
 		self.__startDate = startDate
 		self.__endDate = endDate
-		self.__creator = None
+		self.__creator = creator
 		self.__statement = Enum("Statement", "Creating Checking Active Finished Deleted")
-		self.__parts = []
 		self.__description = None
+		self.__parts = []
 		pass
 
 	@property
@@ -26,10 +26,6 @@ class Course:
 	@property
 	def creator(self):
 		return self.__creator
-	@creator.setter
-	def creator(self, newCreator):
-		self.__creator = newCreator
-		pass
 
 	@property
 	def description(self):
@@ -38,3 +34,34 @@ class Course:
 	def description(self, newDescription):
 		self.__description = newDescription
 		pass
+	pass
+
+
+class Part():
+	def __init__(self, name):
+		self.__name = name
+		self.__problems = []
+		pass
+
+	@property
+	def name(self):
+		return self.__name
+
+	def AddDescription(self, description):
+		self.__description = description
+		pass
+	
+	def AddProblem(self, problemName):
+		self.__problems.append()
+		pass
+	pass
+
+
+class Problem():
+	def __init__(self, name):
+		self.__name = name
+		self.__topic = None
+		self.__solutions = None
+		self.__ratingScale = None
+		pass
+	pass
--- PCMConsoleApp/PCMConsoleApp/PCMConsoleApp.py
+++ PCMConsoleApp/PCMConsoleApp/PCMConsoleApp.py
@@ -1,32 +1,75 @@
-from datetime import datetime
-from CourseClass import Course
+import datetime
+from CourseClass import *
 from UsersClasses import *
+from CommandClass import Command
 
 def ShowHelp(commands):
 	print("\nВозможные команды:")
 	for command in commands:
-		print("{name} — {description}".format(name=command[0], description=command[1]))
+		print(command.GetInfo())
+	print()
 	pass
 
 def ShowCourses():
 	for course in courses:
-		print("{name} from {start} to {end} by {creator}".format(name=course.name, start=course.startDate, end=course.endDate, creator=course.creator))
+		print("{name} from {start} to {end} by {creator}".format(name=course.name, start=course.startDate.strftime(dateFormat), end=course.endDate.strftime(dateFormat), creator=course.creator._User__login))
+	print()
+	pass
+
+def ShowUsers():
+	for user in users:
+		print(user._User__login, user._User__firstName, user._User__lastName, "Student" if type(user) == Student else "Teacher")
+	print()
 	pass
 
 def CreateCourse():
 	def ReadDates():
-		beginString = input("Введите дату начала курса (ДД.ММ.ГГГГ): ")
-		begin = datetime.strptime(beginString, "%d.%m.%Y")
-		endString = input("Введите дату окончания курса (ДД.ММ.ГГГГ): ")
-		end = datetime.strptime(beginString, "%d.%m.%Y")
-		return (begin, end)
+		while (True):
+			startString = input("\tдату начала курса (ДД.ММ.ГГГГ): ")
+			try:
+				start = datetime.datetime.strptime(startString, dateFormat)
+				if (start.date() < datetime.datetime.today().date()):
+					print("Курс не может начаться в прошлом")
+					continue
+			except:
+				print("Это неверная дата")
+				continue
+			
+			endString = input("\tдату окончания курса (ДД.ММ.ГГГГ): ")
+			try:
+				end = datetime.datetime.strptime(endString, dateFormat)
+			except:
+				print("Это неверная дата")
+				continue
 
-	courseName = input("\nВведите название курса: ")
+			if (start < end):
+				break
+			else:
+				print("Курс не может завершиться не начавшись.\n")
+		return (start, end)
+	print("Для создания курса введите...")
+	courseName = input("\tназвание курса: ")
 	courseStart, courseEnd = ReadDates()
-	newCourse = Course(courseName, courseStart, courseEnd)
-	newCourse.description = input("Введите описание для курса:\n")
+	description = input("\tописание для курса: ")
 
-	courses.append(newCourse)
+	newCourse = AddCourse(courseName, courseStart, courseEnd, currentUser, description)
+	WriteData(fileNameCourses, newCourse.name, newCourse.startDate.strftime(dateFormat), newCourse.endDate.strftime(dateFormat), newCourse.creator._User__login, newCourse.description)
+	print("Новый курс \"{0}\" был успешно создан!".format(newCourse.name), end='\n\n')
+	pass
+
+def AddCourse(name, startDate, endDate, creator, description):
+	courses.append(Course(name, startDate, endDate, creator))
+	courses[-1].description = description
+	return courses[-1]
+
+def WriteData(fileName, *args):
+	toFileString = ""
+	for arg in args:
+		toFileString += str(arg) + ';'
+	toFileString = toFileString[:-1] + '\n'
+	fileDescriptor = open(fileName, 'a')
+	fileDescriptor.write(toFileString)
+	fileDescriptor.close()
 	pass
 
 def CreateUser():
@@ -36,51 +79,105 @@ def CreateUser():
 	lastName = input("\tфамилия: ")
 	while (True):
 		role = input("\tдолжность (T - преподаватель, S - студент): ")
-		if (role == 'T'):
-			users.append(Teacher(login, firstName, lastName))
+		if (AddUser(login, firstName, lastName, role)):
 			break
-		elif (role == 'S'):
-			users.append(Student(login, firstName, lastName))
-			break
-		else:
-			print("Выберите T, если вы преподаваетль, либо S, если вы студент. Других вариантов нет.")
-	fileDescriptor = open(fileNameUsers, 'a')
-	fileDescriptor.write(login, firstName, lastName, role, sep=';', )
+	
+	WriteData(fileNameUsers, login, firstName, lastName, role)
+	print("Новый пользователь был успешно создан! Теперь вы можете зайти в аккаунт под логином", login, end='\n\n')
+	pass
+
+def AddUser(login, firstName, lastName, role):
+	isSucces = True
+	if (role == 'T'):
+		users.append(Teacher(login, firstName, lastName))
+	elif (role == 'S'):
+		users.append(Student(login, firstName, lastName))
+	else:
+		isSucces = False
+	return isSucces
+
+def Login(login):
+	isExist = False
+	for user in users:
+		if (login == user._User__login):
+			global currentUser 
+			currentUser = user
+			isExist = True
+			print("Вход выполнен успешно,", currentUser._User__firstName, currentUser._User__lastName, end='\n\n')
+	if (not isExist):
+		print("Пользователя с логином {0} нет в системе. Зарегистрируйтесь с помощью команды /newUser".format(login), end='\n\n')
 	pass
 
 def HandleUserInput():
 	commands = (
-	("/help", "вывод всех допустимых команд"),
-	("/showCourses", "отображение всех имеющихся курсов"),
-	("/newCourse", "запуск помощника по созданию курса"),
-	("/exit", "выход из программы")
+	Command("/help", "вывод всех допустимых команд"),
+	Command("/showCourses", "отображение всех имеющихся курсов"),
+	Command("/showUsers", "отображение всех пользоватлей в системе"),
+	Command("/newCourse", "запуск помощника по созданию курса"),
+	Command("/newUser", "создание нового аккаунта"),
+	Command("/login", "войти в аккаунт"),
+	Command("/go", "прейти в содержиоме курса/раздела/задания"),
+	Command("/exit", "выход из программы")
 	)
 
-	command = input("Введите /help для получения справки по командам или введите команду для исполнения: ")
+	commandsDict = {command.name : command for command in commands}
+
+	beforeLoginMessage = "Перед началом использования программы войдите в свой аккаунт с помощью команды /login вашлогин или зарегистрируйтесь с помощью /newUser"
+	greetingMessage = "Введите /help для получения справки по всем командам (/help -name выведет справку по команде name) или введите команду для исполнения."
+	print(greetingMessage, beforeLoginMessage, sep='\n')
 	while (True):
-		if (command == commands[0][0]):
-			ShowHelp(commands)
-		elif (command == commands[1][0]):
+		command = input("Новая команда: ").split(' ')
+		commandName = command[0]
+		commandAttributes = command[1:]
+
+		if (currentUser == None and (commandName != "/login" and commandName != "/newUser")):
+			print("Войдите в свой аккаунт перед использованием программы с помощью команды /login вашлогин", end='\n\n')
+		
+		elif (commandName == commands[0].name):
+			if (len(commandAttributes) != 0):
+				try:
+					commandHelpName = commandAttributes[0]
+					commandsDict[commandHelpName].PrintInfo()
+				except KeyError:
+					print("Команды \"{0}\" в программе нет.".format(commandAttributes[0]), end='\n\n')
+			else:
+				ShowHelp(commands)
+
+		elif (commandName == commands[1].name):
 			ShowCourses()
-		elif (command == commands[2][0]):
+		elif (commandName == commands[2].name):
+			ShowUsers()
+		elif (commandName == commands[3].name):
 			CreateCourse()
-		elif (command == commands[3][0]):
+		elif (commandName == commands[4].name):
+			CreateUser()
+		elif (commandName == commands[5].name):
+			try:
+				Login(commandAttributes[0])
+			except:
+				Login("")
+		elif (commandName == commands[-1].name):
 			break
 
 		else:
-			print("Команда \"{0}\" неизвестна. Введите действительную команду.".format(command))
-		command = input("Новая команда: ")
+			print("Команда \"{0}\" неизвестна. Введите действительную команду.".format(commandName), end='\n\n')
 	pass
 
 
 currentUser = None
+currentUserPosition = None
 courses = []
 users = []
 fileNameUsers = "DataFiles/Users.txt"
 fileNameCourses = "DataFiles/Courses.txt"
-currentPosition = None
+
+dateFormat = "%d.%m.%Y"
+
 
 def Main():
+	open(fileNameUsers, 'a').close()
+	open(fileNameCourses, 'a').close()
+
 	users = GetUsersFromFile()
 	courses = GetCoursesFromFile()
 	
@@ -93,10 +190,27 @@ def GetUsersFromFile():
 	fileDescriptor = open(fileNameUsers)
 	rawData = fileDescriptor.readlines()
 	for line in rawData:
-		login, firstName, lastName, role = line.split(';')
+		login, firstName, lastName, role = line.replace('\n','').split(';')
+		AddUser(login, firstName, lastName, role)
+	fileDescriptor.close()
 	pass
 
 def GetCoursesFromFile():
+	fileDescriptor = open(fileNameCourses)
+	rawData = fileDescriptor.readlines()
+	for line in rawData:
+		name, start, end, creator, description = line.replace('\n','').split(';')
+		startDate = datetime.datetime.strptime(start, dateFormat)
+		endDate = datetime.datetime.strptime(end, dateFormat)
+		AddCourse(name, startDate, endDate, GetCreator(creator), description)
+	fileDescriptor.close()
 	pass
+
+def GetCreator(login):
+	creator = None
+	for user in users:
+		if (user._User__login == login):
+			creator = user
+	return creator
 
 Main()
